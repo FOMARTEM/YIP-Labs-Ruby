c5022dc1a26b55aea75712a99edbbf34a6a58f7f
Artem Shahov <shahov9ad@gmail.com>
2024-04-05 13:15:25 +0300

    done main and user files

---
+++ lab5/var-31-1/lab5_main.rb
@@ -0,0 +1,39 @@
+module PartOne
+    def self.calculate_y (x)
+        y = (1 - (1 +(Math.sin(x)).abs)**0.5) / (2 + Math.cos(x)**2)
+    end
+end
+
+module PartTwo
+    def self.isPalindrome(s)
+        down_s = s.downcase
+        down_s == down_s.reverse
+    end
+end
+
+module PartTree
+    def self.korrector_strok(strings)
+        counter_corrected = 0
+        corrected_strings = []
+        strings.each do |string|
+            corrected_string = ''
+            string.split.map do |word|
+                #puts word
+                if  word.match(/^[a-zA-Z0-9]+$/)
+                    if word.match(/^[0-9]/)
+                        counter_corrected += 1
+                        word = "_" + word[1..-1]
+                    end
+                    #puts word
+                    corrected_string << word + ' '
+                end
+            end
+            corrected_strings << corrected_string
+        end
+        puts "Исходные строки: #{strings}"
+        puts "Скорректированные строки: #{corrected_strings}"
+        puts "Количество корректировок: #{counter_corrected}"           
+    end
+end
+
+
---
+++ lab5/var-31-1/lab5_user.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative './lab5_main'
+
+loop do
+    puts "Выберите номер задания: [1], [2], [3]\nИли напишите [выход/exit], чтобы выйти"
+    case gets.chomp
+    when '1'
+      puts 'Введите x:'
+      x = gets.chomp.to_f
+      puts "Результат вычисления функции: #{PartOne.calculate_y x}"
+    when '2'
+      puts 'Введите строку:'
+      inp = gets.chomp
+      if PartTwo.isPalindrome(inp)
+        puts "Строка является палиндромом"
+      else
+        puts "Строка не является палиндромом"
+      end
+    when '3'
+      puts 'Введтте последовательность строк из слов, разделённых пробелмами:'
+      strings = []
+      loop do
+        string = gets.chomp
+        if string.empty?
+            break
+        end
+        strings << string
+      end
+      puts "Делаю магические преобразования"
+      PartTree.korrector_strok(strings)
+      
+    when 'выход'
+      puts 'Завершаю работу'
+      break
+    when 'exit'
+      puts 'Завершаю работу'
+      break
+    else
+      puts 'Некорректный выбор'
+    end
+    puts
+end
+  
